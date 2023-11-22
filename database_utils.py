0e9e46e3bc187f05504d3e5773fc9c0e14861f13
plainsman <yiannissanozidis@hotmail.co.uk>
2023-11-22 23:45:36 +0000

    Add empty function and tried to change other things, failed

--- database_utils.py
+++ database_utils.py
@@ -1,35 +1,36 @@
 import yaml
 import psycopg2
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 
 class DatabaseConnector:
+
     def read_db_cred():
         with open('db_creds.yaml', 'r') as credentials:
             db_cred = yaml.safe_load(credentials)
             return db_cred
     
+
     def init_db_engine(self):
         creds = DatabaseConnector.read_db_cred()
-        DATABASE_TYPE = 'postgresql'
-        DBAPI = 'psycopg2'
-        USER = creds['RDS_USER']
-        PASSWORD = creds['RDS_PASSWORD']
-        HOST = creds['RDS_HOST']
-        PORT = creds['RDS_PORT']
-        DATABASE = creds['RDS_DATABASE']
-        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+        self.DATABASE_TYPE = 'postgresql'
+        self.DBAPI = 'psycopg2'
+        self.USER = creds['RDS_USER']
+        self.PASSWORD = creds['RDS_PASSWORD']
+        self.HOST = creds['RDS_HOST']
+        self.PORT = creds['RDS_PORT']
+        self.DATABASE = creds['RDS_DATABASE']
+        self.engine = create_engine(f"{self.DATABASE_TYPE}+{self.DBAPI}://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}")
+        self.engine.execution_options(isolation_level='AUTOCOMMIT').connect()
     
+
     def list_db_tables(self):
         self.init_db_engine()
-        with psycopg2.connect(host=HOST, user=USER, password=PASSWORD, dbname=DATABASE, port=PORT) as conn:
+        with psycopg2.connect(host=self.HOST, user=self.USER, password=self.PASSWORD, dbname=self.DATABASE, port=self.PORT) as conn:
             with conn.cursor() as cur:
-                cur.execute('''SELECT table_name FROM information_schema.tables
-                                WHERE table_schema = public''')
+                cur.execute("""SELECT table_name FROM information_schema.tables
+                                WHERE table_schema = 'public'""")
                 for table in cur.fetchall():
                     print(table)
 
-
-
-
-connector = DatabaseConnector()
-
+    def upload_to_db(data_frame, table_name):
+        pass
