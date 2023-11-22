b02a0589e55c93e647628a94e0d7f35bdf7b8673
plainsman <yiannissanozidis@hotmail.co.uk>
2023-11-22 23:46:10 +0000

    Made read_rds_Table function

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,21 @@
-class DataExtractor:
-    pass
+import database_utils
+import pandas as pd
+
+class DataExtractor(database_utils.DatabaseConnector):
+
+    def __init__(self):
+        self.init_db_engine()
+
+    def read_rds_table(self, table_name):
+        self.data = pd.read_sql_table(f"{table_name}", self.engine)
+        return self.data
+
+    
+extractor = DataExtractor()
+extractor.list_db_tables()
+table = input("Enter table name: ")
+data = extractor.read_rds_table(table)
+print(data.head())
+
+
+
