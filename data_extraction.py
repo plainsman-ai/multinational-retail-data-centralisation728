ac3c7087f641c83df52fd1442cc92f97dd093a14
plainsman <yiannissanozidis@hotmail.co.uk>
2023-11-25 14:05:41 +0000

    Accidentally restored older commit but kept current copy so now am back on that one

--- data_extraction.py
+++ data_extraction.py
@@ -1,21 +1,25 @@
 import database_utils
 import pandas as pd
+import tabula
 
 class DataExtractor(database_utils.DatabaseConnector):
 
-    def __init__(self):
-        self.init_db_engine()
+    # def __init__(self):
+        # self.init_db_engine()
 
     def read_rds_table(self, table_name):
-        self.data = pd.read_sql_table(f"{table_name}", self.engine)
-        return self.data
-
+        data = pd.read_sql_table(f"{table_name}", self.engine)
+        self.engine.close()
+        return data
     
-extractor = DataExtractor()
-extractor.list_db_tables()
-table = input("Enter table name: ")
-data = extractor.read_rds_table(table)
-print(data.head())
+## open the above with python in command line
+# need to import pandas, put option to show all columns
+
+    def retrieve_pdf_data(self, link):
+        pass
+
+
+
 
 
 
