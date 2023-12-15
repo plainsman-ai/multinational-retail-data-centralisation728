6aa495fcae24d040ca791312e35f3566977c7b45
plainsman <yiannissanozidis@hotmail.co.uk>
2023-12-15 21:44:06 +0000

    Last changes to data_cleaning as well as documentation

--- data_cleaning.py
+++ data_cleaning.py
@@ -9,6 +9,7 @@ class DataCleaning(data_extraction.DataExtractor):
         ### step 1 - cleaning legacy_users table
         legacy_users_data = self.read_rds_table("legacy_users")
 
+        # removes bogus rows and corrects country_code
         legacy_users_data["country_code"].replace({'GGB':'GB'}, inplace=True) # (replaces GGB w GB)
         legacy_users_data = legacy_users_data[legacy_users_data.country_code.isin(["GB", "DE", "US"])] # removes junk data
 
@@ -19,6 +20,7 @@ class DataCleaning(data_extraction.DataExtractor):
         legacy_users_data["join_date"] =  legacy_users_data["join_date"].apply(parse)
         legacy_users_data["join_date"] = pd.to_datetime(legacy_users_data["join_date"], errors='coerce')
         
+        # removes an unnecessary index table
         legacy_users_data = legacy_users_data.drop("index", axis=1)
         
         return legacy_users_data
@@ -26,42 +28,70 @@ class DataCleaning(data_extraction.DataExtractor):
 
     
     def clean_card_data(self):
+
         card_data = self.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
         card_data = card_data[0]    # extract dataframe from list
 
+        # removes title rows from the pdf pages
+        card_data = card_data[card_data["card_number"] != "card_number"]
+
+        # removes a lot of bogus data
+        card_providers = ["VISA 16 digit", "JCB 16 digit", "VISA 13 digit", "JCB 15 digit", "VISA 19 digit", "Diners Club / Carte Blanche", "American Express", "Maestro", "Discover", "Mastercard"]
+        card_data = card_data[card_data["card_provider"].isin(card_providers)]
+
+        # makes dates conform to datetime
+        card_data["date_payment_confirmed"] =  card_data["date_payment_confirmed"].apply(parse)
         card_data["date_payment_confirmed"] = pd.to_datetime(card_data["date_payment_confirmed"], errors='coerce')
 
-        card_data = card_data.dropna()  # drops all rows w empty data and makes columns equal
+        # cleans the card number column
+        card_data["card_number"] = card_data["card_number"].str.extract('(\d+)', expand=False)
 
+        # corrects index after removed rows
         card_data = card_data.reset_index(drop=True)
 
         return card_data
     
 
+# for dim_store_details table
+
+# NB removed a step where "lat" column is dropped
+
+#   stores_data = stores_data.drop("lat", axis=1)
+
+# NB another step where nulll values are dropped
+
+#   stores_data = stores_data.dropna()
+
+
 
     def clean_stores_data(self):
+
         stores_data = self.retrieve_stores_data()
 
-        stores_data = stores_data.drop("lat", axis=1)
-
+        # removes bogus code and corrects continent labels
         stores_data["continent"].replace({'eeEurope':'Europe', 'eeAmerica': 'America'}, inplace=True)
         stores_data = stores_data[stores_data["continent"].isin(["Europe", "America"])]
         
+        # makes opening_date conform to datetime
         stores_data["opening_date"] =  stores_data["opening_date"].apply(parse)
         stores_data["opening_date"] = pd.to_datetime(stores_data["opening_date"], errors='coerce')
 
-        stores_data = stores_data.dropna()
-
+        # makes index revert after rows removed
         stores_data = stores_data.drop("index", axis=1)
         stores_data = stores_data.reset_index(drop=True)
+
+        # removes all alphabet letters from the staff_numbers, returning only integer values
+        stores_data["staff_numbers"] = stores_data["staff_numbers"].astype('str').str.extractall('(\d+)').unstack().fillna('').sum(axis=1).astype(int)
 
         return stores_data
     
 
 
     def convert_product_weights(self):
+        # this is the first step for the products data
         product_weights = self.extract_from_s3()
-        
+
+        # this is a formula for making weight combinations conform to wanted categories
         def convert(weight):
             if type(weight) == float:
                 return weight
@@ -83,6 +113,8 @@ class DataCleaning(data_extraction.DataExtractor):
             else:
                 return weight
             
+        # thjs applies the function above to the column
+
         product_weights["weight"] = product_weights["weight"].apply(convert)
 
         return product_weights
@@ -92,11 +124,14 @@ class DataCleaning(data_extraction.DataExtractor):
     def clean_products_data(self):
         products_data = self.convert_product_weights()
 
+        #drops an unneeded column
         products_data = products_data.drop("Unnamed: 0", axis=1)
 
+        # corrects a spelling error and removes bogus data
         products_data["removed"].replace({"Still_avaliable": "Still_available"}, inplace=True)
         products_data = products_data[products_data["removed"].isin(["Still_available", "Removed"])]
 
+        # conform to datetime
         products_data["date_added"] =  products_data["date_added"].apply(parse)
         products_data["date_added"] = pd.to_datetime(products_data["date_added"], errors='coerce')
 
@@ -106,6 +141,7 @@ class DataCleaning(data_extraction.DataExtractor):
     def clean_orders_data(self):
         orders_data = self.read_rds_table("orders_table")
 
+        # drops unneeded columns
         orders_data = orders_data.drop(["1", "first_name", "last_name", "level_0", "index"], axis=1)
 
         return orders_data
@@ -113,12 +149,14 @@ class DataCleaning(data_extraction.DataExtractor):
     def clean_sales_data(self):
         sales_data = self.extract_sales_file()
 
+        # drops bogus data and resets index to new rows
         sales_data = sales_data[sales_data["time_period"].isin(["Evening", "Midday", "Morning", "Late_Hours"])]
         sales_data = sales_data.reset_index(drop=True)
 
         return sales_data
 
     
+# i never used this bc it would need to be changed each time and i just used the terminal
 if __name__ == "__main__":
     cleaner = DataCleaning()
     sales_data = cleaner.clean_sales_data()
