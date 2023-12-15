679b2497d3a0ac7874a0f8d1a6268805abb0c74c
plainsman <yiannissanozidis@hotmail.co.uk>
2023-12-15 21:44:52 +0000

    Documentation on database_utils

--- database_utils.py
+++ database_utils.py
@@ -5,12 +5,13 @@ from sqlalchemy import create_engine, inspect
 
 class DatabaseConnector:
 
+    # credentials are stored in a yaml file so this opens it
     def read_db_cred():
         with open('db_creds.yaml', 'r') as credentials:
             db_cred = yaml.safe_load(credentials)
             return db_cred
     
-
+    # connects to a database on AWS  and makes an engine object for requests
     def init_db_engine(self):
         creds = DatabaseConnector.read_db_cred()
         self.DATABASE_TYPE = 'postgresql'
@@ -23,22 +24,21 @@ class DatabaseConnector:
         self.engine = create_engine(f"{self.DATABASE_TYPE}+{self.DBAPI}://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}")
         self.engine.execution_options(isolation_level='AUTOCOMMIT').connect()
     
-
+    # lists table names from database in previous step
     def list_db_tables(self):
         self.init_db_engine()
         inspector = inspect(self.engine)
         table_names = inspector.get_table_names()
         print(table_names)
 
-    def upload_to_db(self, my_dframe, table_name):
-        my_dframe.to_sql(f'{table_name}', self.engine, if_exists='replace')
-
-
+    # reads my yaml file 
     def read_my_db_cred():
          with open('pword_yaml.yaml', 'r') as my_creds:
             db_cred = yaml.safe_load(my_creds)
             return db_cred
-    
+         
+    # used to upload to local postgresql database
+    # NB i use this to upload directly from terminal
     def upload_to_db(self, my_dframe, table_name):
         my_pword = DatabaseConnector.read_my_db_cred()
         self.engine = create_engine(f"{self.DATABASE_TYPE}+{self.DBAPI}://{'postgres'}:{my_pword}@{'localhost'}:{5432}/{'sales_data'}")
